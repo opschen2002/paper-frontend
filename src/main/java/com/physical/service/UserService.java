97fbdacc3eb395af746b6213bd999fa739d7c173
fight <2902121271@qq.com>
2025-04-26 17:08:24 +0800

    null

--- src/main/java/com/physical/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.physical.service;
-
-import com.physical.dto.LoginDTO;
-import com.physical.entity.User;
-
-public interface UserService {
-    User login(LoginDTO loginDTO);
-    boolean validateUser(String username, String password, String role);
-}
